3c54bcac24b1afaeb2fe9fbe057c911311b57ddb
visualstudio <visualstudio@122-PC17>
2019-02-01 19:15:59 -0500

    C0727191-CSD3354-WEEK04

--- C0727191-CSD3354-WEEK-04/Program.cs
+++ C0727191-CSD3354-WEEK-04/Program.cs
@@ -10,6 +10,34 @@ namespace C0727191_CSD3354_WEEK_04
     {
         static void Main(string[] args)
         {
+
+            countryside c = new countryside();
+
         }
+    }
+    class village
+    {
+        public bool isAstrildeHere;
+        public village nextvillage;
+        public village prevvillage;
+        public String villagename;
+
+
+    }
+
+
+    class countryside
+    {
+        village Maple;
+        village Toronto;
+        village Ajax;
+
+        public void run()
+        {
+            Maple = new village();
+            Maple.villagename = "Toronto";
+            Maple.nextvillage= Toronto;
+        }
+        
     }
 }
